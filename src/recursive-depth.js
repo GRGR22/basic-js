0bcfdda26d2ea2b077a1ae55a39ee40bd9c1b8cd
GR22 <bagbox@mail.ru>
2021-03-12 17:49:47 +0400

    fead: work with recursiv

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,7 +2,19 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let arrDepth = 0;
+    let newArr = [];
+    let i = false;
+    do {
+      newArr = arr.flat(1);
+      console.log(newArr);
+      if (newArr.length > arr.length) {
+        arrDepth++;
+        arr = arr.flat(1);
+      } else {
+        i = true
+      };
+    } while (i);
+    return arrDepth;
   }
 };
