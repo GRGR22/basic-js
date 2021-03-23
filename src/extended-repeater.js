ffaccf5626960fb1255f7c79b6ef6432ae1a5b00
GR22 <bagbox@mail.ru>
2021-03-23 11:13:51 +0400

    feat: add ext-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (typeof str !== 'string') str+='';
+  let duplicate = '', answer = '', separator = '+', addition = undefined, additionSeparator = '|';   
+  (options.separator != undefined) ? separator = options.separator: '';
+  (options.addition != undefined) ? addition = options.addition + '': '';  
+  (options.addition === null) ? addition = 'null': ''; 
+  (options.additionSeparator != undefined) ? additionSeparator = options.additionSeparator: '';
+  if (addition != undefined) {
+    if (options.additionRepeatTimes != undefined) {
+      ((options.additionRepeatTimes < 2) && (options.additionRepeatTimes == 1)) ? duplicate += addition: duplicateManipulation ();
+      } else duplicate += addition;
+    }
+  if (options.repeatTimes != undefined) {
+    if (options.repeatTimes < 2) {
+      ((options.repeatTimes == 1) && (options.additionRepeatTimes == undefined)) ? answer += str: answerManipulation ();       
+    } else answerManipulation ();
+  } else answer = str + addition;
+
+  function answerManipulation () {
+    answer = (str + duplicate + separator).repeat(options.repeatTimes - 1)+(answer + str + duplicate);
+  }
+  function duplicateManipulation () {
+    duplicate = (addition + additionSeparator).repeat(options.additionRepeatTimes - 1)+addition;
+  }
+  return answer;
 };
   
